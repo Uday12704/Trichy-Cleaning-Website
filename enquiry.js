3a86c22b5eb4b450e5e364aeb02c58c5c7ff5518
Yogesh Limbani <yrlimbani03@gmail.com>
2024-01-25 12:58:18 +0530

    Update enquiry-form

--- enquiry.js
+++ enquiry.js
@@ -6,7 +6,6 @@ message = document.getElementById("message");
 sendBtn = document.getElementById("sendBtn");
 
 fullAlert = document.querySelector(".full-alert");
-fullString = document.querySelector(".full-string");
 nameAlert = document.querySelector(".name-alert");
 phoneAlert = document.querySelector(".phone-alert");
 messageAlert = document.querySelector(".message-alert");
@@ -14,17 +13,14 @@ messageAlert = document.querySelector(".message-alert");
 successAlert = document.querySelector(".success-alert");
 failAlert = document.querySelector(".fail-alert");
 
-fullAlertString = "Form submission failed. Review the following information: "
-review = [];
+review = 0;
 
 function checkName() {
     let firstNameValue = firstName.value.trim();
     firstNameValue == "" ? firstName.style.backgroundColor = "#ffeae8" : firstName.style.backgroundColor = "white";
     if (firstNameValue === "") {
         nameAlert.style.display = "flex";
-        if (!("Name" in review)) {
-            review.push("Name");
-        }
+        review = 1;
         return false;
     }
     nameAlert.style.display = "none";
@@ -35,9 +31,7 @@ function checkPhone() {
     let phoneValue = phone.value.trim();
     if (phoneValue === "") {
         phoneAlert.style.display = "flex";
-        if (!("Phone" in review)) {
-            review.push("Phone");
-        }
+        review = 1;
         return false;
     }
     phoneAlert.style.display = "none";
@@ -48,10 +42,8 @@ function checkMessage() {
     let messageValue = message.value.trim();
     if (messageValue === "") {
         messageAlert.style.display = "flex";
-        if (!("Message" in review)) {
-            review.push("Message");
-        }
         message.style.backgroundColor = "#ffeae8";
+        review = 1;
         return false;
     }
     messageAlert.style.display = "none";
@@ -63,10 +55,8 @@ sendBtn.addEventListener("click", () => {
     checkName();
     checkPhone();
     checkMessage();
-    if (review.length > 0) {
+    if (review === 1) {
         fullAlert.style.display = "flex";
-        fullString.innerText = fullAlertString + review.join(", ") + ".";
-        review = [];
     }
     else {
         fullAlert.style.display = "none";
@@ -80,7 +70,7 @@ function sendMail() {
     let emailValue = email.value.trim();
     let messageValue = message.value.trim();
 
-    emailjs.init("364zyalzO9rVbmoUMvyB5");
+    emailjs.init("364zyalzO9rVbmoUMvyB5"); //private key
 
     const formData = {
         name: firstNameValue,
