77224a514f8bdc656845fc24612a7c6c587584e2
Yogesh Limbani <yrlimbani03@gmail.com>
2024-01-28 11:59:13 +0530

    Fix Enquiry-Form

--- enquiry.js
+++ enquiry.js
@@ -16,82 +16,90 @@ failAlert = document.querySelector(".fail-alert");
 review = 0;
 
 function checkName() {
-    let firstNameValue = firstName.value.trim();
-    firstNameValue == "" ? firstName.style.backgroundColor = "#ffeae8" : firstName.style.backgroundColor = "white";
-    if (firstNameValue === "") {
-        nameAlert.style.display = "flex";
-        review = 1;
-        return false;
-    }
-    nameAlert.style.display = "none";
-    return true;
+  let firstNameValue = firstName.value.trim();
+  firstNameValue == ""
+    ? (firstName.style.backgroundColor = "#ffeae8")
+    : (firstName.style.backgroundColor = "white");
+  if (firstNameValue === "") {
+    nameAlert.style.display = "flex";
+    review = 1;
+    return false;
+  }
+  review = 0;
+  nameAlert.style.display = "none";
+  return true;
 }
 
 function checkPhone() {
-    let phoneValue = phone.value.trim();
-    if (phoneValue === "") {
-        phoneAlert.style.display = "flex";
-        review = 1;
-        return false;
-    }
-    phoneAlert.style.display = "none";
-    return true;
+  let phoneValue = phone.value.trim();
+  if (phoneValue === "") {
+    phoneAlert.style.display = "flex";
+    review = 1;
+    return false;
+  }
+  review = 0;
+  phoneAlert.style.display = "none";
+  return true;
 }
 
 function checkMessage() {
-    let messageValue = message.value.trim();
-    if (messageValue === "") {
-        messageAlert.style.display = "flex";
-        message.style.backgroundColor = "#ffeae8";
-        review = 1;
-        return false;
-    }
-    messageAlert.style.display = "none";
-    message.style.backgroundColor = "white";
-    return true;
+  let messageValue = message.value.trim();
+  if (messageValue === "") {
+    messageAlert.style.display = "flex";
+    message.style.backgroundColor = "#ffeae8";
+    review = 1;
+    return false;
+  }
+  review = 0;
+  messageAlert.style.display = "none";
+  message.style.backgroundColor = "white";
+  return true;
 }
 
 sendBtn.addEventListener("click", () => {
-    checkName();
-    checkPhone();
-    checkMessage();
-    if (review === 1) {
-        fullAlert.style.display = "flex";
-    }
-    else {
-        fullAlert.style.display = "none";
-        sendMail();
-    }
+  checkName();
+  checkPhone();
+  checkMessage();
+  if (review === 1) {
+    fullAlert.style.display = "flex";
+  } else {
+    fullAlert.style.display = "none";
+    sendMail();
+  }
 });
 
 function sendMail() {
-    let firstNameValue = firstName.value.trim();
-    let phoneValue = phone.value.trim();
-    let emailValue = email.value.trim();
-    let messageValue = message.value.trim();
+  let firstNameValue = firstName.value.trim();
+  let phoneValue = phone.value.trim();
+  let emailValue = email.value.trim();
+  let messageValue = message.value.trim();
 
-    emailjs.init("364zyalzO9rVbmoUMvyB5"); //private key
+  emailjs.init("364zyalzO9rVbmoUMvyB5"); //private key
 
-    const formData = {
-        name: firstNameValue,
-        email: emailValue,
-        phone: phoneValue,
-        message: messageValue
-    };
+  const formData = {
+    name: firstNameValue,
+    email: emailValue,
+    phone: phoneValue,
+    message: messageValue,
+  };
 
-    emailjs.send("service_eay6c5q", "template_x7ddo8r", formData,"NnhSdAb1IoHTWFN-Z")
-        .then(function (response) {
-            successAlert.style.display = "flex";
-            resetVals();
-        }, function (error) {
-            failAlert.style.display = "flex";
-            resetVals();
-        });
+  emailjs
+    .send("service_eay6c5q", "template_x7ddo8r", formData, "NnhSdAb1IoHTWFN-Z")
+    .then(
+      function (response) {
+        successAlert.style.display = "flex";
+        resetVals();
+      },
+      function (error) {
+        failAlert.style.display = "flex";
+        resetVals();
+      }
+    );
 }
 
 function resetVals() {
-    firstName.value = "";
-    phone.value = "";
-    email.value = "";
-    message.value = "";
+  firstName.value = "";
+  phone.value = "";
+  email.value = "";
+  message.value = "";
 }
